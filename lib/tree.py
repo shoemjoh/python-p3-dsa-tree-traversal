8b925501a52f2633319263d3de8f42877af9b990
John Shoemaker <shoemjoh@gmail.com>
2024-10-20 21:12:24 -0400

    working traversal lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    node = self.root
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      current_node = nodes_to_visit.pop(0)
+
+      if current_node['id'] == id:
+        return current_node
+      else:
+        nodes_to_visit = current_node['children'] + nodes_to_visit
+
+    return None
+
